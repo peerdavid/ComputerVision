3737628262dd8c73ef809fc9f5476eac5951895c
david.peer <david@peer.tirol>
2015-11-20 22:23:49 +0100

    Some tests with gabor filter

--- app/src/main/java/tirol/peer/david/computervision/MainActivity.java
+++ app/src/main/java/tirol/peer/david/computervision/MainActivity.java
@@ -1,19 +1,42 @@
 package tirol.peer.david.computervision;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
+    static {
+        // If you use opencv 2.4, System.loadLibrary("opencv_java")
+        System.loadLibrary("opencv_java3");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -22,11 +45,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -49,4 +77,59 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            ImageView imageView = (ImageView) findViewById(R.id.ImgView);
+            EditText textEdit = (EditText) findViewById(R.id.EditText);
+
+            try {
+                Bitmap origBmp = getBitmapFromUri(selectedImage);
+                Mat origImage = new Mat (origBmp.getWidth(), origBmp.getHeight(), CvType.CV_8UC1);
+                Utils.bitmapToMat(origBmp, origImage);
+
+                double sigma = Double.valueOf(textEdit.getText().toString());
+
+                Mat newImage = gaborFilter(origImage,sigma);
+                Bitmap newBmp = origBmp;
+                Utils.matToBitmap(newImage, newBmp);
+
+                imageView.setImageBitmap(newBmp);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Mat gaborFilter(Mat img, double theta) {
+        Size kSize = new Size(31,31);
+
+        double lambda = 30;
+        double sigma = 24;
+        double gamma = 1;
+        double psi =  0;
+
+        Mat kernel = Imgproc.getGaborKernel(kSize, sigma, theta, lambda, gamma, psi, CvType.CV_32F);
+
+        Mat gabor = new Mat (img.width(), img.height(), CvType.CV_8UC1);
+        Imgproc.filter2D(img, gabor, -1, kernel);
+
+        return gabor;
+    }
+
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
 }
